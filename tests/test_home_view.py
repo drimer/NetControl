1fc64d89d9162adaff5d2e0b87735961725d4380
Alberto Aguilera <drimer.aaj@gmail.com>
2015-09-24 11:29:45 +0100

    Add a simple test as a reference

---
+++ tests/test_home_view.py
@@ -0,0 +1,12 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from webapp.views import home
+
+__author__ = 'alberto'
+
+
+class HomeViewTest(TestCase):
+    def test_home_view_renders_correctly(self):
+        response = self.client.get(reverse(home))
+        self.assertEqual(200, response.status_code)
